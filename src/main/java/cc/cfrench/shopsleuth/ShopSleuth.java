d164252228f32843d843e08dbcb3e5d44ccd2f25
Cole French <16979554+ColeFrench@users.noreply.github.com>
2023-01-31 14:44:32 -0500

    Update mod metadata

--- src/main/java/cc/cfrench/shopsleuth/ShopSleuth.java
+++ src/main/java/cc/cfrench/shopsleuth/ShopSleuth.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package cc.cfrench.shopsleuth;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ShopSleuth.MODID, version = ShopSleuth.VERSION)
+public class ShopSleuth
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "shopsleuth";
+    public static final String VERSION = "0.1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
