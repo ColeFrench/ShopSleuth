6bed11c88705712e0ba35d577e26b36d9319b2dd
Cole French <16979554+ColeFrench@users.noreply.github.com>
2023-02-02 18:30:06 -0500

    Clear example initialization

--- src/main/java/cc/cfrench/shopsleuth/ShopSleuth.java
+++ src/main/java/cc/cfrench/shopsleuth/ShopSleuth.java
@@ -1,20 +1,15 @@
 package cc.cfrench.shopsleuth;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ShopSleuth.MODID, version = ShopSleuth.VERSION)
-public class ShopSleuth
-{
+public class ShopSleuth {
     public static final String MODID = "shopsleuth";
     public static final String VERSION = "0.1.0";
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void init(FMLInitializationEvent event) {
     }
 }
