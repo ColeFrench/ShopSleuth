7c4264ed72a52857a667cae66d4bea7d92e7454b
Cole French <16979554+ColeFrench@users.noreply.github.com>
2023-02-08 16:42:50 -0500

    Track resource counts

--- src/main/java/cc/cfrench/shopsleuth/player/Inventory.java
+++ src/main/java/cc/cfrench/shopsleuth/player/Inventory.java
@@ -1,14 +1,26 @@
 package cc.cfrench.shopsleuth.player;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class Inventory {
+    private final Map<Item, Long> resourceCounts = new HashMap<>();
     private ItemStack lastMousedItemStack;
+
+    public long getResourceCount(final Item item) {
+        return this.resourceCounts.getOrDefault(item, 0L);
+    }
 
     public Optional<ItemStack> getLastMousedItemStack() {
         return Optional.ofNullable(this.lastMousedItemStack);
+    }
+
+    protected void setResourceCounts(final Map<? extends Item, ? extends Long> resourceCounts) {
+        this.resourceCounts.putAll(resourceCounts);
     }
 
     protected void setLastMousedItemStack(final ItemStack lastMousedItemStack) {
--- src/main/java/cc/cfrench/shopsleuth/player/InventoryListener.java
+++ src/main/java/cc/cfrench/shopsleuth/player/InventoryListener.java
@@ -5,19 +5,50 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InventoryListener {
+    private static final Set<Item> RESOURCE_ITEMS = Stream.of(
+            "minecraft:iron_ingot",
+            "minecraft:gold_ingot",
+            "minecraft:emerald",
+            "minecraft:diamond")
+        .map(Item::getByNameOrId)
+        .collect(Collectors.collectingAndThen(
+            Collectors.toCollection(HashSet::new),
+            Collections::unmodifiableSet));
+
     private final Status status;
     private final Inventory inventory;
 
     public InventoryListener(final Status status, final Inventory inventory) {
         this.status = status;
         this.inventory = inventory;
+    }
+
+    @SubscribeEvent
+    public void onPlayerTick_UpdateResourceCounts(final TickEvent.PlayerTickEvent event) {
+        if (event.player instanceof EntityPlayerSP
+                && this.status.isChallengeActive()
+                && event.phase == TickEvent.Phase.END) {
+            final Stream<ItemStack> resourceStream = Stream.concat(
+                    Arrays.stream(event.player.inventory.mainInventory),
+                    Stream.of(event.player.inventory.getItemStack()))
+                .filter(Objects::nonNull)
+                .filter(itemStack -> RESOURCE_ITEMS.contains(itemStack.getItem()));
+            final Map<Item, Long> resourceCounts = resourceStream.collect(
+                Collectors.groupingBy(ItemStack::getItem, Collectors.mapping(
+                    itemStack -> itemStack.stackSize,
+                    Collectors.counting())));
+            this.inventory.setResourceCounts(resourceCounts);
+        }
     }
 
     @SubscribeEvent
